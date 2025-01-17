a84ec862854d12fde1f9f2e93b865cfbb83fc262
Ryan Spencer <ryan.spencer26@d155.org>
2025-01-17 08:10:42 -0600

    made custom cell class

---
+++ CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  MovieAppSpencer
+//
+//  Created by RYAN SPENCER on 1/17/25.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell{
+    
+//    @IBOutlet weak var movieLabel: UILabel!
+//
+//    @IBOutlet weak var favButton: UITextView!
+//
+//    @IBOutlet weak var watchlistButton: UITextView!
+    
+    func configure(_ movie: String){
+       
+    }
+    
+}
--- MovieAppSpencer/SearchViewController.swift
+++ MovieAppSpencer/SearchViewController.swift
@@ -7,18 +7,25 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var searchField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        getMovie("Blade")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func getMovie(_ movie: String){
         
         let session = URLSession.shared
-        let url = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=78557f93&s=\(movie)")
+        let url = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=78557f93&type=movie&s=\(movie)")
         let dataTask = session.dataTask(with: url!){ data, response, error in
             if error != nil{
                 print("Error")
@@ -39,5 +46,29 @@ class SearchViewController: UIViewController {
         dataTask.resume()
         
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! CustomCell
+        cell.configure(movies[indexPath.row])
+        return cell
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+        if searchField.text != "" && searchField.text != "The" {
+            getMovie(searchField.text!)
+            tableView.reloadData()
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Movie not found.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 }
