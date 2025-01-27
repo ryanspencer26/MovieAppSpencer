8849fa2175d7f332576a21f69d011902ecb2efe0
Ryan Spencer <ryan.spencer26@d155.org>
2025-01-27 08:51:21 -0600

    finish using struct to display info

--- InfoViewController.swift
+++ InfoViewController.swift
@@ -25,6 +25,52 @@ class InfoViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         titleLabel.text = AppData.currentMovie
+        getMovie(AppData.currentMovie)
     }
+    
+    func getMovie(_ movie: String){
+        
+        let session = URLSession.shared
+        let url = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=78557f93&t=\(movie)")
+        let dataTask = session.dataTask(with: url!){ data, response, error in
+            if error != nil{
+                let alert = UIAlertController(title: "Error", message: "Movie not found.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(okAction)
+                DispatchQueue.main.async {
+                    self.present(alert, animated: true, completion: nil)
+                }
+                return
+            } else {
+                if let d = data {
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as? NSDictionary{
+                        if jsonObj.value(forKey: "Error") as? String == "Movie not found!"{
+                            let alert = UIAlertController(title: "Error", message: "Movie not found.", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alert.addAction(okAction)
+                            DispatchQueue.main.async {
+                                self.present(alert, animated: true, completion: nil)
+                            }
+                            return
+                        }
+                        if let year = jsonObj.value(forKey: "Year"){
+                            print("\(movie) was released in \(year)")
+                            DispatchQueue.main.async {
+                                //textField
+                            }
+                        }
+                    } else {
+                        print("json object is nil")
+                    }
+                } else {
+                    print("No data")
+                }
+            }
+        }
+        
+        dataTask.resume()
+        
+    }
+    
 
 }
